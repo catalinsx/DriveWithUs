14782ecfc252fced447b056c5aa78af945127c07
catalinsx <tcatalin1337@gmail.com>
2023-10-22 19:34:00 +0300

    am facut Dashboard.java care urmeaza sa fie practic Main-ul programului, adaugat "resource images" si am facut Dashboard.form, foarte chinuit.

---
+++ src/Dashboard.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+
+public class Dashboard {
+    private JPanel dashboardPanel;
+    private JLabel imageLabel;
+    private JLabel imageLogoLabel;
+    private JComboBox comboBox1;
+    private JCheckBox doYouHaveACheckBox;
+    private JButton searchForACarButton;
+
+    public Dashboard(){
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Dashboard");
+        Dashboard dashboard = new Dashboard();
+        frame.setContentPane(dashboard.dashboardPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon imageIcon = new ImageIcon("src/Images/lexusRemove.png");
+        ImageIcon imageLogoIcon = new ImageIcon("src/Images/Drivewithus.png");
+        dashboard.imageLogoLabel.setIcon(imageLogoIcon);
+        dashboard.imageLabel.setIcon(imageIcon);
+        frame.setVisible(true);
+        frame.setSize(1366, 768 );
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+    }
+}
