11c00102c5bd5817addc857e7e539b1f7f17026d
SerbanPrecup <precup.serban@yahoo.com>
2023-10-25 21:47:15 +0300

    Am modificat clasele,am adaugat o interfata si clasa Car a devenit clasa abstracta, fiind mostenita de clasele Luxury, Sport si Premium.

---
+++ src/rentacar/cars/Vehicle.java
@@ -0,0 +1,48 @@
+package rentacar.cars;
+//S:Clasa abstracta Vehicle
+public abstract class Vehicle {
+    private String registrationNumber;
+    private String brand;
+    private String model;
+    private int year;
+    private double dailyPrice;
+//S:constructor public explicit
+    public Vehicle(String registrationNumber, String brand, String model, int year, double dailyPrice)
+    {
+        this.registrationNumber = registrationNumber;
+        this.brand=brand;
+        this.model=model;
+        this.year=year;
+        this.dailyPrice=dailyPrice;
+    }
+
+    public String getRegistrationNumber(){
+        return this.registrationNumber;
+    }
+    public void setRegistrationNumber(String registrationNumber){
+        this.registrationNumber = registrationNumber;
+    }
+    public String getbrand(){
+        return this.brand;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public double getDailyPrice(){
+        return this.dailyPrice;
+    }
+
+    //S: metoda abstracta pentru calcularea chiriei
+    public abstract double calculateRentalCost(int days);
+
+    @Override
+    public String toString(){
+        return "Registration Number: " + registrationNumber + "\nbrand: " + brand + "\nModel: " + model + "\nYear: " + year + "\nDaily Price: " + dailyPrice;
+    }
+}
